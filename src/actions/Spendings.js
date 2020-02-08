21b95c4b10924a8fe53d4230e48746f4f14d2e48
Goshak95 <goshak2005@yandex.ru>
2020-02-09 02:28:58 +0300

    createa async actions and corresponding reducers

--- src/actions/Spendings.js
+++ src/actions/Spendings.js
@@ -1,8 +1,44 @@
-export const GET_SPENDINGS = 'GET_SPENDINGS'
+export const GET_SPENDINGS_REQUEST = 'GET_SPENDINGS_REQUEST'
+export const GET_SPENDINGS_SUCCESS = 'GET_SPENDINGS_SUCCESS'
+export const GET_SPENDINGS_FAILURE = 'GET_SPENDINGS_FAILURE'
+
+export const ADD_SPENDING_REQUEST = 'ADD_SPENDING_REQUEST'
+export const ADD_SPENDING_SUCCESS = 'ADD_SPENDING_SUCCESS'
+export const ADD_SPENDING_FAILURE = 'ADD_SPENDING_FAILURE'
 
 export function getSpendings(spendingsList) {
-  return {
-    type: GET_SPENDINGS,
-    payload: spendingsList,
+  return dispatch => {
+    dispatch({
+      type: GET_SPENDINGS_REQUEST,
+    })
+
+    fetch('https://5e3ed04a64c3f60014550d95.mockapi.io/api/v1/coinsleft/spendings')
+      .then(response => response.json())
+      .then(data => dispatch({ type: GET_SPENDINGS_SUCCESS, payload: data }))
+      .catch(error => dispatch({ type: GET_SPENDINGS_FAILURE, payload: error.message }))
+  }
+}
+
+export function addSpending(spendingData) {
+  return dispatch => {
+    dispatch({
+      type: ADD_SPENDING_REQUEST,
+    })
+    console.log(JSON.stringify(spendingData))
+    fetch('https://5e3ed04a64c3f60014550d95.mockapi.io/api/v1/coinsleft/spendings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(spendingData),
+    })
+      .then(response => response.json())
+      .then(data =>
+        dispatch({
+          type: ADD_SPENDING_SUCCESS,
+          payload: data,
+        })
+      )
+      .catch(error => dispatch({ type: ADD_SPENDING_FAILURE, payload: error.message }))
   }
 }
--- src/reducers/spendings.js
+++ src/reducers/spendings.js
@@ -1,16 +1,37 @@
-import { GET_SPENDINGS } from '../actions/Spendings'
+import {
+  GET_SPENDINGS_SUCCESS,
+  GET_SPENDINGS_REQUEST,
+  GET_SPENDINGS_FAILURE,
+  ADD_SPENDING_REQUEST,
+  ADD_SPENDING_SUCCESS,
+  ADD_SPENDING_FAILURE,
+} from '../actions/Spendings'
 
 const initialState = {
   error: null,
-  isLoaded: false,
+  isLoading: false,
   spendingsList: [],
 }
 
 export function SpendingsReducer(state = initialState, action) {
   switch (action.type) {
-    case GET_SPENDINGS:
-      return { ...state, spendingsList: action.payload, isLoaded: true }
+    case GET_SPENDINGS_REQUEST:
+      return { ...state, isLoading: true }
+    case GET_SPENDINGS_SUCCESS:
+      return { ...state, spendingsList: action.payload, isLoading: false }
+    case GET_SPENDINGS_FAILURE:
+      return { ...state, error: action.payload }
+
+    case ADD_SPENDING_REQUEST:
+      return { ...state, isLoading: true }
+    case ADD_SPENDING_SUCCESS:
+      const newSpendings = state.spendingsList.concat()
+      newSpendings.push(action.payload)
+      return { ...state, spendingsList: newSpendings, isLoading: false }
+    case ADD_SPENDING_FAILURE:
+      return { ...state, error: action.payload }
+
     default:
-      return { ...state }
+      return state
   }
 }
