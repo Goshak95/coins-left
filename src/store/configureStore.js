2517b8bf83256aa3f643cefc9ae5d4f2b864b457
Goshak95 <goshak2005@yandex.ru>
2020-02-09 02:25:38 +0300

    implement redux-thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,12 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { rootReducer } from '../reducers'
 import logger from 'redux-logger'
+import ReduxThunk from 'redux-thunk'
 
-export const store = createStore(rootReducer, applyMiddleware(logger))
+export const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(ReduxThunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
