06045e2816f37a10b7b0b9b9508e295770c3fdf2
Goshak95 <goshak2005@yandex.ru>
2020-02-11 23:45:05 +0300

    add 404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Navigation } from './components/Navigation'
 import { routes } from './routes'
 import './App.scss'
+import { NotFound } from './components/NotFound'
 
 class App extends React.Component {
   renderRoutes = () => {
@@ -26,7 +27,10 @@ class App extends React.Component {
             <Navigation />
           </header>
           <main className="content">
-            <Switch>{this.renderRoutes()}</Switch>
+            <Switch>
+              {this.renderRoutes()}
+              <Route component={NotFound} />
+            </Switch>
           </main>
         </div>
       </Router>
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.scss'
+
+export const NotFound = ({ message }) => {
+  return (
+    <div className="not-found">
+      <h1>THE P4GE Y0U 4RE LOOKING FOR WAS STOLEN BY THE UFO</h1>
+      <p>Sorry for that.</p>
+    </div>
+  )
+}
